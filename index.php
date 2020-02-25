cde626c5ea147da05041e68dda4bcfbb1e891dc8
Albert-C-Ong <albert_ong_us@yahoo.com>
2020-02-24 19:52:48 -0800

    Basic prototype and assets
    Added basic assets, including concepts for the logo design and favicon, and created the basis for the login and dashboard pages

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<html>
+
+<head>
+  <title>Lyfestyle</title>
+  <link rel="icon" type="image/png" href="assets/Lyfestyle_favicon.png">
+</head>
+
+<body>
+<h1>Lyfestyle</h1>
+
+Username:<input type="text"><br>
+Password: <input type="text"><br>
+
+<input type="submit" onClick="location.href='pages/dashboard.php'" value="Login">
+  
+</body>
+  
+</html>
+
+
---
+++ pages/dashboard.php
@@ -0,0 +1,18 @@
+
+<html>
+
+<head>
+  <title>Lyfestyle | Dashboard</title>
+  <link rel="icon" type="image/png" href="assets/Lyfestyle_favicon.png">
+</head>
+
+<body>
+<h1>Welcome!</h1>
+
+<input type="submit" onClick="location.href='../index.php'" value="Logout">
+  
+</body>
+  
+</html>
+
+
